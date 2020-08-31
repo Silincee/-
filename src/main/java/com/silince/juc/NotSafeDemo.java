0feeb8c2b209dac69ea8fbbc94a6a1763fde377e
Silince <837418086@qq.com>
2020-08-31 12:15:08 +0800

    CopyOnWriteArrayList

---
+++ src/main/java/com/silince/juc/NotSafeDemo.java
@@ -0,0 +1,28 @@
+package main.java.com.silince.juc;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @program: 多线程高并发
+ * @description: 请举例说明集合类是不安全的
+ * @author: Silince
+ * @create: 2020-08-30 20:48
+ **/
+public class NotSafeDemo {
+    public static void main(String[] args) {
+//        List<String> list = new ArrayList<>(); //ArrayList线程不安全
+//        List<String> list = new Vector<>(); //Vector 线程安全
+//        List<Object> list = Collections.synchronizedList(new ArrayList<>());
+        List<String> list = new CopyOnWriteArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                // method
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+
+    }
+}
